0d82e1968d44f3193f24810ff57ce7aad6ed3ff3
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2014-01-23 22:10:01 +0530

    Initial Check in

---
+++ LogReader/log.js
@@ -0,0 +1,14 @@
+﻿var file = require('./fileio');
+var express = require('express');
+
+var app = express();
+
+//app.configure(function() {
+//    //app.use(express.logger('dev'));
+//    //app.use(express.bodyParser());
+//});
+
+app.get('/log', file.getLogs);
+
+app.listen(1100);
+console.log('listening port 1100');
