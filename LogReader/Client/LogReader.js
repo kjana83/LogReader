1aa778aa7ae16e6634027478afab85da34aacbca
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2014-01-26 20:46:16 +0530

    Client added

---
+++ LogReader/Client/LogReader.js
@@ -0,0 +1,25 @@
+﻿var logReader = angular.module('LogReader',[]);
+
+logReader.controller('LogController', function($scope,logService) {
+    $scope.ViewModel = logService;
+    $scope.ViewModel.Init();
+});
+
+logReader.service('logService', function($http) {
+    var viewModel = function() {
+        var self = this;
+        this.LogData = [];
+        this.ReadLogCommand = function() {
+            
+            $http.get('/log').success(function(data,status) {
+                console.log(data);
+                self.LogData = angular.fromJson(data);
+            });
+        };
+        this.Init = function() {
+            self.ReadLogCommand();
+        };
+    };
+
+    return new viewModel();
+});
---
+++ LogReader/Index.js
@@ -0,0 +1,19 @@
+﻿var express = require('express');
+var file = require('./Server/fileio');
+
+var app = express();
+
+//app.configure(function() {
+//    //app.use(express.logger('dev'));
+//    //app.use(express.bodyParser());
+//});
+
+
+app.configure(function() {
+    app.use(express.favicon());
+    app.use(express.static(__dirname));
+});
+
+app.get('/log', file.getLogs);
+
+app.listen(1100);
